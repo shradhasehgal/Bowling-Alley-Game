f68a5ef373a9303941081c2ef0e69a96e38a0b4d
Shradha <shradha.sgl@gmail.com>
2020-04-09 01:54:26 +0530

    Fixed cohesion in endgame prompt

--- src/EndGamePrompt.java
+++ src/EndGamePrompt.java
@@ -19,9 +19,7 @@ public class EndGamePrompt implements ActionListener {
 
 	private JFrame win;
 	private int result;
-
-	private String selectedNick, selectedMember;
-
+	
 	public EndGamePrompt( String partyName ) {
 
 		result =0;
