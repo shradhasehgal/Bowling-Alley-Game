e6abf8bf85c2082a322f338cc3a4542fa16401dc
Shradha <shradha.sgl@gmail.com>
2020-05-10 22:12:29 +0530

    Removed codemr history

--- codemr/A3 19/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"A3", "Number of Highly Problematic Classes":0, "Number of Entities":36, "Number of Problematic Classes":0, "Class Lines of Code":1325, "Number of External Packages":18, "Number of Packages":1, "Number of External Entities":89}
